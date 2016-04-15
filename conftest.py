a10322539b0435644c3f66b61588a247d06e6a1d
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-04-15 08:20:00 +0200

    introduced tagged wrapper to allow command line filtering of the fixtures

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import _pytest
+from functools import wraps
 
 
 def pytest_addoption(parser):
@@ -8,32 +9,45 @@ def pytest_addoption(parser):
 		default=None, help="filter fixtures by tag")
 
 
-def apply_fixture(request, fixture_fn):
-	raw_tags = request.config.getoption('tags')
-	if not raw_tags:
-		return True
-	required_tags = set(raw_tags.split(','))
-	# TODO: can implement shouldn't have tags relying on ! or ~ prefix
-	fn_tags = set(fixture_fn.tags)
-	missing_req = required_tags - fn_tags
-	if missing_req:
-		return False
-	return True
+class tagged(object):
 
-@pytest.fixture(scope='module')
+	def __init__(self, **tags):
+		assert len(tags) == 1, 'usage: tagged(<tag name>=["tag1", "tag2"])'
+		(self.tag_name, provides), = tags.items()
+		self.tags = set(provides)
+
+	def __call__(self, func):
+		@wraps(func)
+		def inner(request, *a, **kw):
+			raw_tags = request.config.getoption(self.tag_name)
+			if raw_tags:
+				required_tags = set(raw_tags.split(','))
+				# TODO: can implement shouldn't have tags relying on ! or ~ prefix
+				missing_req = required_tags - self.tags
+				if missing_req:
+					_pytest.runner.skip('not applicable due to tags')
+			return func(request, *a, **kw)
+		return inner
+
+
+@pytest.fixture
+@tagged(tags=['enduser', 'billing'])
 def end_user_billing_only(request):
-	if not apply_fixture(request, end_user_billing_only):
-		_pytest.runner.skip('not applicable due to tags')
 	return dict(billing='billing', delivery=None)
-end_user_billing_only.tags = ['enduser', 'billing']
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture
+@tagged(tags=['enduser', 'billing', 'delivery'])
 def end_user_billing_and_delivery(request):
 	return dict(billing='billing', delivery='delivery')
-end_user_billing_and_delivery.tags = ['enduser', 'billing', 'delivery']
 
 
-@pytest.fixture(params=[end_user_billing_only, end_user_billing_and_delivery])
+@pytest.fixture
+@tagged(tags=['partner', 'billing', 'delivery'])
+def partner_billing_and_delivery(request):
+	return dict(partner='some partner', billing='billing', delivery='delivery')
+
+
+@pytest.fixture(params=[end_user_billing_only, end_user_billing_and_delivery, partner_billing_and_delivery])
 def checkout_address(request):
 	return request.param(request)
