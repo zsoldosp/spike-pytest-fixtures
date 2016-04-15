5c1e3c3a224e0a7eb78a827316015d1c40e7cb7e
Peter Zsoldos <peter.zsoldos@gmail.com>
2016-04-15 08:32:29 +0200

    pytest_addoption automatically generated from tagged decorators applied

--- conftest.py
+++ conftest.py
@@ -3,18 +3,18 @@ import _pytest
 from functools import wraps
 
 
-def pytest_addoption(parser):
-        parser.addoption(
-		"--tags", action="store", dest='tags',
-		default=None, help="filter fixtures by tag")
-
-
 class tagged(object):
+
+	options = {}
 
 	def __init__(self, **tags):
 		assert len(tags) == 1, 'usage: tagged(<tag name>=["tag1", "tag2"])'
 		(self.tag_name, provides), = tags.items()
 		self.tags = set(provides)
+		tagged.options.setdefault(self.tag_name, set([]))
+		tagged.options[self.tag_name] |= self.tags
+		missing = self.tags - tagged.options[self.tag_name]
+		assert len(missing) == 0, missing
 
 	def __call__(self, func):
 		@wraps(func)
@@ -51,3 +51,13 @@ def partner_billing_and_delivery(request):
 @pytest.fixture(params=[end_user_billing_only, end_user_billing_and_delivery, partner_billing_and_delivery])
 def checkout_address(request):
 	return request.param(request)
+
+
+def pytest_addoption(parser):
+	for option_name, values in tagged.options.items():
+		arg_name = '--%s' % option_name.replace('_', '-')
+		parser.addoption(
+			arg_name, action="store", dest=option_name,
+			# TODO: choices would be cool, but need to learn how to accept multi value
+			#choices=values,
+			default=None, help="filter %s fixtures (tags)" % option_name)
